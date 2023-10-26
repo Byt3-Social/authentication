f611a86052fa2d31431616e06e0c2e077a04c6ba
Lucas-Welter <boltwelter@gmail.com>
2023-10-25 23:48:51 -0400

    docs: documentação da api realizada com o springdoc

--- src/main/java/com/byt3social/authentication/AuthenticationApplication.java
+++ src/main/java/com/byt3social/authentication/AuthenticationApplication.java
@@ -3,7 +3,19 @@ package com.byt3social.authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+
+
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(
+        title = "API Autenticação",
+        version = "1.0",
+        description = "Api destinada a autenticação na plataforma por parte das organizações."
+    )
+)
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/byt3social/authentication/controllers/OrganizacaoController.java
+++ src/main/java/com/byt3social/authentication/controllers/OrganizacaoController.java
@@ -4,6 +4,14 @@ import com.byt3social.authentication.dto.OrganizacaoLoginDTO;
 import com.byt3social.authentication.models.Organizacao;
 import com.byt3social.authentication.models.OrganizacaoToken;
 import com.byt3social.authentication.services.OrganizacaoService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,25 +22,34 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/organizacao")
+@Tag(name = "Organizacao Controller", description = "API para operações relacionadas a organizações")
 public class OrganizacaoController {
     @Autowired
     private AuthenticationManager authenticationManager;
+
     @Autowired
     private OrganizacaoService organizacaoService;
 
-    @PostMapping("/organizacao/login")
+    @Operation(summary = "Autenticar uma organização")
+    @ApiResponse(responseCode = "200", description = "Autenticação bem-sucedida", content = @Content(schema = @Schema(implementation = OrganizacaoToken.class)))
+    @ApiResponse(responseCode = "401", description = "Autenticação mal-sucedida")
+    @PostMapping("/login")
     public ResponseEntity login(@RequestBody OrganizacaoLoginDTO organizacaoLoginDTO) {
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(organizacaoLoginDTO.usuario(), organizacaoLoginDTO.senha());
 
         Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
 
-       OrganizacaoToken token = new OrganizacaoToken(organizacaoService.gerarTokenJWT((Organizacao) authentication.getPrincipal()));
+        OrganizacaoToken token = new OrganizacaoToken(organizacaoService.gerarTokenJWT((Organizacao) authentication.getPrincipal()));
 
         return new ResponseEntity(token, HttpStatus.OK);
     }
 
-    @PostMapping("/organizacao/validar")
-    public ResponseEntity validarToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization) {
+    @Operation(summary = "Validar o token de uma organização")
+    @ApiResponse(responseCode = "200", description = "Token válido", content = @Content(schema = @Schema(implementation = Organizacao.class)))
+    @ApiResponse(responseCode = "401", description = "Token inválido")
+    @PostMapping("/validar")
+    public ResponseEntity validarToken(@RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(description = "Token JWT no formato 'Bearer <token>'", example = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9...") String authorization) {
         String token = authorization.replace("Bearer ", "");
         Boolean tokenValido = organizacaoService.validarTokenJWT(token);
 
